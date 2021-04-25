15aa6cfc4b0ccea954d43c9105a35c4fc02b91cc
Konstantin <kostya.battlefield13@mail.ru>
2021-04-25 15:58:48 +0400

    add moves for both players
    and display which one is play

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -8,25 +8,43 @@ namespace TicTacToe
 {
     class Program
     {
-        static char[,] field = new char[3, 3];
+        static int[,] field = new int[3, 3] { { 3, 3, 3 }, { 3, 3, 3 }, { 3, 3, 3} }; // Чтобы массив был пустым при отрисовке, заполняем его 
+                                                                    //любыми значениями, кроме 1 и 0 потому что это коды для крестика и нолика.
+        const int PLAYER_X = 1;
+        const int PLAYER_O = 0;
 
-        const char PLAYER_X = 'X';
-        const char PLAYER_O = 'O';
+        static int current = PLAYER_X;
 
         static void Main(string[] args)
         {
             for (int i = 0; i < 9; i++)
             {
                 DrawField();
+                Console.WriteLine(" Ходит " + GetPlayer(current));
                 Console.WriteLine(" Введите X.");
                 int x = int.Parse(Console.ReadLine());
                 Console.WriteLine(" Введите Y.");
                 int y = int.Parse(Console.ReadLine());
-                field[x - 1, y - 1] = PLAYER_X;
+                field[x - 1, y - 1] = current;
+                current = (current == 1) ? 0 : 1;
             }
         }
 
-        public static void DrawField()
+        static string GetPlayer(int player)
+        {
+            switch (player)
+            {
+                case PLAYER_O: {
+                    return "O";
+                    }
+                case PLAYER_X: {
+                    return "X";
+                    }
+                default: return " ";
+            }
+        }
+
+        static void DrawField()
         {
             Console.Clear();
             Console.WriteLine();
@@ -34,7 +52,7 @@ namespace TicTacToe
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    Console.Write(" " + field[i, j] + " ");                   
+                    Console.Write(" " + GetPlayer(field[i, j]) + " ");                   
                 }
                 Console.WriteLine();
             }
